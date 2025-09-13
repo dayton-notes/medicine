5c9c5bccc117d7d493f547f4c23013f2244fd1e4
dayton-notes <adayton1@gmail.com>
2025-09-12 20:21:08 -0400

    update

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -67,7 +67,7 @@ export interface GlobalConfiguration {
   /** Base URL to use for CNAME files, sitemaps, and RSS feeds that require an absolute URL.
    *   Quartz will avoid using this as much as possible and use relative URLs most of the time
    */
-  baseUrl?: string
+  baseUrl?: baseUrl: "https://dayton-notes.github.io/medicine"
   theme: Theme
   /**
    * Allow to translate the date in the language of your choice.
