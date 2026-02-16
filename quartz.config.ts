6358d167b33d72a37f00b6568f0cc8e977dee540
dayton-notes <adayton1@gmail.com>
2026-02-16 13:33:04 -0500

    Auto-update: synced 6 Medicine folders

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "📖 dtn.notes",
+    pageTitle: "📖 D.N",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
